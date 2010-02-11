54390145a8ca8385d0658134f7544358f83b8510
Tim Akinbo <takinbo@gmail.com>
2010-02-12 00:04:47 +0100

    fixed a typo and added CharField restriction settings

--- apps/campaigns/models.py
+++ apps/campaigns/models.py
@@ -1,17 +1,17 @@
 # vim: ai sts=4 ts=4 et sw=4
 from django.db import models
-from location.models import Location
+from locations.models import Location
 
 # Create your Django models here, if you need them.
 class Application(models.Model):
-    name = models.CharField()
-    description = models.CharField()
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
 
     def __unicode__(self):
         return self.name
 
 class Campaign(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=255)
     locations = models.ManyToManyField(Location)
     start_date = models.DateField()
     end_date = models.DateField()
