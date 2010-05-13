cca3813278f2707d66dd8ae9b5e585c57aec4b8c
Tim Akinbo <takinbo@gmail.com>
2010-05-13 19:00:15 +0100

    added support for registrations

--- apps/ipd/formslogic.py
+++ apps/ipd/formslogic.py
@@ -22,8 +22,8 @@ class IPDFormsLogic(FormsLogic):
             "display" : "Report",
             "fields": (
                 ("location", "location"),
-                ("immunized", "immunized"),
                 ("commodity", "commodity"),
+                ("immunized", "immunized"),
             )
         },
 
@@ -54,7 +54,52 @@ class IPDFormsLogic(FormsLogic):
         form_entry = args[1]
         # in case we need help, build a valid reminder string
         # TODO put this in the db!
-        if form_entry.form.code.abbreviation == "report":
+        required = ["location", "role", "firstname"]
+        help = ("%s register " % form_entry.domain.code.abbreviation.lower()) +\
+                " ".join(["<%s>" % t for t in required])
+        if form_entry.form.code.abbreviation == "register":
+            data = form_entry.to_dict()
+
+            # check that ALL FIELDS were provided
+            missing = [t for t in required if data[t] is None]
+            
+            # missing fields! collate them, and
+            # send back a friendly non-localized
+            # error message, then abort
+            if missing:
+                mis_str = ", ".join(missing)
+                return ["Missing fields: %s" % mis_str, help]
+            
+            # parse the name via Reporter
+            flat_name = data.pop("firstname") + " " + data.pop("secondname") + " " + data.pop("thirdname")
+            data["alias"], data["first_name"], data["last_name"] =\
+                Reporter.parse_name(flat_name.strip())
+            
+            # all fields were present and correct, so copy them into the
+            # form_entry, for "actions" to pick up again without re-fetching
+            form_entry.rep_data = data
+            
+            # parse the roles out. 
+            # TODO: how can this be done generically
+            role_code = data.pop("role")
+            role = None
+            try:
+                role = Role.objects.get(code__iexact=role_code)
+            except Role.DoesNotExist:
+                # try to match the pattern
+                for db_role in Role.objects.all():
+                    if db_role.match(role_code):
+                        role = db_role
+                        break
+            if not role:
+                return ["Unknown role code: %s" % role_code]
+            data["role"] = role
+            
+            
+            # nothing went wrong. the data structure
+            # is ready to spawn a Reporter object
+            return None
+        elif form_entry.form.code.abbreviation == "report":
             required = ['location', 'immunized', 'commodity']
             data = form_entry.to_dict()
 
@@ -92,8 +137,42 @@ class IPDFormsLogic(FormsLogic):
     def actions(self, *args, **kwargs):
         message = args[0]
         form_entry = args[1]
+        
+        if form_entry.form.code.abbreviation== "register":
 
-        if self._form_lookups.has_key(form_entry.form.code.abbreviation):
+            data = form_entry.rep_data
+            # load the location and role objects via their codes
+            data["location"] = Location.objects.get(code__iexact=data["location"])
+            # this happens in validation now
+            # data["role"]     = Role.objects.get(code__iexact=data["role"])
+            
+            # spawn and save the reporter using the
+            # data we collected in self.validate
+            rep = Reporter(**data)
+            conn = PersistantConnection.from_message(message)
+            # check for duplicate registration.
+            if Reporter.exists(rep, conn):
+                # if they already exist just re-send a confirmation but don't
+                # create a new instance. 
+                message.respond("Hello again %s! You are already registered as a %s at %s %s."\
+                                % (rep.first_name, rep.role, rep.location, rep.location.type), StatusCodes.OK)
+                return
+            # if they didn't exist then save them
+            rep.save()
+
+            # we can assume that the new reporter will be using
+            # this device again, so register a connection. this
+            # automatically logs them in, so they can start
+            # reporting straight away
+            conn.reporter = rep
+            conn.save()
+
+            # notify the user that everyting went okay
+            # TODO: proper (localized?) messages here
+            message.respond("Hello %s! You are now registered as %s at %s %s."\
+                % (rep.first_name, rep.role, rep.location, rep.location.type), StatusCodes.OK)
+
+        elif self._form_lookups.has_key(form_entry.form.code.abbreviation):
             to_use = self._form_lookups[form_entry.form.code.abbreviation]
             form_class = to_use["class"]
             field_list = to_use["fields"]
