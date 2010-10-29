100b9989867c382dc225a7a3f483d247927f1db9
Tim Akinbo <takinbo@gmail.com>
2010-10-29 03:05:26 +0100

    added the SMS forms processing for the VLM app
    also committed the model definitions for storing the
    vlm data

--- apps/vlm/app.py
+++ apps/vlm/app.py
@@ -2,17 +2,221 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 import rapidsms
+from rapidsms.parsers.keyworder import *
+from models import *
+from reporters.models import PersistantConnection, Reporter
+from datetime import datetime, timedelta
 
 class App(rapidsms.app.App):
+
+    kw = Keyworder()
+    error_msgs = {
+        'invalid_location': "Sorry, I don't have the facility with the code: %s in my database. Please confirm and try again.",
+        'invalid_commodity': "Hmm... I don't have any recollection of a commodity with the code: %s. I only know the following commodities: %s"
+        }
+
     def start(self):
         pass
 
     def parse(self, message):
+        self.handled = False
         pass
 
     def handle(self, message):
-        pass
+        try:
+            if hasattr(self, "kw"):
+                try:
+                    func, captures = self.kw.match(self, message.text)
+                    func(self, message, *captures)
+                    return self.handled
+                except Exception, e:
+                    return self.handled
+            else:
+                self.debug('App has not instantiated keyworder as kw')
+        except Exception, e:
+            self.error(e)
 
     def cleanup(self, message):
         pass
 
+    kw.prefix = ['vlm']
+
+    @kw("help")
+    def help(self, message):
+        message.respond(['receive', 'origin'])
+        self.handled = True
+
+    @kw("(i|issue) from (slug) to (slug) (slug) (slug) (whatever) (numbers) (numbers) (whatever)")
+    def issue(self, message, command, origin, destination, commodity, batch, expiry, qty, bal, vvmstatus):
+        try:
+            origin_facility = Facility.objects.get(code=origin)
+        except Facility.DoesNotExist:
+            message.respond(self.error_msgs['invalid_location'] % origin)
+            self.handled = True
+            return True
+
+        try:
+            destination_facility = Facility.objects.get(code=destination)
+        except Facility.DoesNotExist:
+            message.respond(self.error_msgs['invalid_location'] % destination)
+            self.handled = True
+            return True
+
+        if not commodity in dict(PartialTransaction.COMMODITIES):
+            message.respond(self.error_msgs['invalid_commodity'] % (
+                commodity, ", ".join(dict(PartialTransaction.COMMODITIES).keys())))
+            self.handled = True
+            return True
+
+        # partial transaction
+        try:
+            ten_hrs_ago = datetime.now() - timedelta(0, 36000, 0)
+            pt = PartialTransaction.objects.get(origin__code=origin, destination__code=destination, commodity=commodity, batch=batch, type='I', date__gte=ten_hrs_ago, connection=PersistantConnection.from_message(message))
+            pt.date = datetime.now()
+            pt.expiry = self.convert_date(expiry)
+            pt.amount = qty
+            pt.stock = bal
+            pt.save()
+        except PartialTransaction.DoesNotExist:
+            pt = PartialTransaction()
+            pt.connection = PersistantConnection.from_message(message)
+            pt.reporter = pt.connection.reporter
+            pt.origin = Facility.objects.get(code=origin)
+            pt.destination = Facility.objects.get(code=destination)
+            pt.commodity = commodity
+            pt.expiry = self.convert_date(expiry)
+            pt.batch = batch
+            pt.amount = qty
+            pt.stock = bal
+            pt.date = datetime.now()
+            pt.type = 'I'
+            pt.save()
+
+        # stock for facility
+        try:
+            stock = Stock.objects.get(facility__code=origin)
+            stock.balance = bal
+            stock.save()
+        except Stock.DoesNotExist:
+            stock = Stock()
+            stock.facility = Facility.objects.get(code=origin)
+            stock.balance = bal
+            stock.save()
+
+        message.respond('Report received for VLM ISSUE: from %s to %s COMMODITY: %s, EXPIRY: %s, DOSES: %s, STOCK: %s, VVMSTATUS: %s' % (
+            origin_facility, destination_facility, commodity, expiry, qty,
+            bal, vvmstatus
+        ))
+        self.handled = True
+
+    def convert_date(self, date):
+        return datetime.strptime(date, '%d/%m/%Y').date()
+
+    @kw("(r|receive) from (slug) to (slug) (slug) (slug) (whatever) (numbers) (numbers) (whatever)")
+    def receive(self, message, command, origin, destination, commodity, batch, expiry, qty, bal, vvmstatus):
+        try:
+            origin_facility = Facility.objects.get(code=origin)
+        except Facility.DoesNotExist:
+            message.respond(self.error_msgs['invalid_location'] % origin)
+            self.handled = True
+            return True
+
+        try:
+            destination_facility = Facility.objects.get(code=destination)
+        except Facility.DoesNotExist:
+            message.respond(self.error_msgs['invalid_location'] % destination)
+            self.handled = True
+            return True
+
+        if not commodity in dict(PartialTransaction.COMMODITIES):
+            message.respond(self.error_msgs['invalid_commodity'] % (
+                commodity, ", ".join(dict(PartialTransaction.COMMODITIES).keys())))
+            self.handled = True
+            return True
+
+        # partial transaction
+        try:
+            ten_hrs_ago = datetime.now() - timedelta(0, 36000, 0)
+            pt_receive = PartialTransaction.objects.get(origin__code=origin, destination__code=destination, commodity=commodity, batch=batch, type='R', date__gte=ten_hrs_ago, connection=PersistantConnection.from_message(message))
+            pt_receive.date = datetime.now()
+            pt_receive.expiry = self.convert_date(expiry)
+            pt_receive.amount = qty
+            pt_receive.stock = bal
+            pt_receive.save()
+        except PartialTransaction.DoesNotExist:
+            pt_receive = PartialTransaction()
+            pt_receive.connection = PersistantConnection.from_message(message)
+            pt_receive.reporter = pt_receive.connection.reporter
+            pt_receive.origin = Facility.objects.get(code=origin)
+            pt_receive.destination = Facility.objects.get(code=destination)
+            pt_receive.commodity = commodity
+            pt_receive.expiry = self.convert_date(expiry)
+            pt_receive.batch = batch
+            pt_receive.amount = qty
+            pt_receive.stock = bal
+            pt_receive.date = datetime.now()
+            pt_receive.type = 'R'
+            pt_receive.save()
+
+        # stock for facility
+        try:
+            stock = Stock.objects.get(facility__code=destination)
+            stock.balance = bal
+            stock.save()
+        except Stock.DoesNotExist:
+            stock = Stock()
+            stock.facility = Facility.objects.get(code=destination)
+            stock.balance = bal
+            stock.save()
+
+        # find matching previous partial transaction
+        try:
+            pt_issue = PartialTransaction.objects.get(origin=origin_facility, destination=destination_facility, commodity=commodity, batch=batch, type='I')
+
+            # shipment
+            try:
+                shipment = Shipment.objects.get(origin=origin_facility, destination=destination_facility, commodity=commodity, batch=batch)
+                shipment.expiry = self.convert_date(expiry)
+                shipment.sent = pt_issue.date
+                shipment.received = pt_receive.date
+            except Shipment.DoesNotExist:
+                shipment = Shipment()
+                shipment.origin = origin_facility
+                shipment.destination = destination_facility
+                shipment.commodity = commodity
+                shipment.expiry = self.convert_date(expiry)
+                shipment.sent = pt_issue.date
+                shipment.received = pt_receive.date
+                shipment.batch = batch
+                shipment.save()
+
+            # transaction
+            try:
+                trans = Transaction.objects.get(shipment=shipment,receipt=pt_receive)
+                trans.amount_received = qty
+                trans.receipt = pt_receive
+                trans.save()
+            except Transaction.DoesNotExist:
+                trans = Transaction()
+                trans.amount_sent = pt_issue.amount
+                trans.amount_received = pt_receive.amount
+                trans.shipment = shipment
+                trans.issue = pt_issue
+                trans.receipt = pt_receive
+                trans.save()
+
+        except PartialTransaction.DoesNotExist:
+            # no matching issue partial transaction found
+            # could we possibly be trying to receive without an issue?
+            pass
+
+        message.respond('Report received for VLM RECEIVE: from %s to %s COMMODITY: %s, EXPIRY: %s, DOSES: %s, STOCK: %s, VVMSTATUS: %s' % (
+            origin_facility, destination_facility, commodity, expiry, qty,
+            bal, vvmstatus
+        ))
+        self.handled = True
+
+    @kw('(whatever)')
+    def default(self, message, text):
+        message.respond("We didn't understand your message.")
+        self.handled = True
---
+++ apps/vlm/models.py
@@ -0,0 +1,102 @@
+# vim: ai sts=4 ts=4 et sw=4
+from django.db import models
+from reporters.models import Reporter, PersistantConnection
+from locations.models import Location, Facility
+
+class PartialTransaction(models.Model):
+    COMMODITIES = (
+        ('opv', 'Oral Polio Vaccine'),
+        ('vita', 'Vitamin A'),
+        ('tt', 'Tetanus Toxoid'),
+        ('mv', 'Measles Vaccine'),
+        ('bcg', 'Bacille Calmette-Guerin Vaccine'),
+        ('yf', 'Yellow Fever'),
+        ('hepb', 'Hepatitis B'),
+        ('fe', 'Iron Folate'),
+        ('dpt', 'Diphtheria'),
+        ('deworm', 'Deworming'),
+        ('sp', 'Sulphadoxie Pyrimethanol for IPT'),
+        ('plus', 'Plus'),
+    )
+    TRANSACTION_TYPES = (
+        ('I', 'Issue'),
+        ('R', 'Receipt'),
+    )
+    STATUS_TYPES = (
+        ('P', 'Pending'),
+        ('C', 'Confirmed'),
+        ('A', 'Amended'),
+    )
+    FLAG_TYPES = (
+        ('S', 'Reported stock does not match expected stock balance.'),
+        ('U', 'Unregistered reporter.'),
+    )
+
+    reporter = models.ForeignKey(Reporter, blank=True, null=True, related_name='vlm_partialtransactions')
+    connection = models.ForeignKey(PersistantConnection, blank=True, null=True, related_name="vlm_partialtransactions")
+    origin = models.ForeignKey(Facility, related_name='origins')
+    destination = models.ForeignKey(Facility, related_name='destinations')
+    commodity = models.CharField(blank=True, null=True, max_length=10, choices=COMMODITIES)
+    expiry = models.DateField(db_index=True)
+    batch = models.CharField(max_length=15, blank=True, null=True, help_text="Waybill number")
+    amount = models.PositiveIntegerField(blank=True, null=True, help_text="Amount of supply shipped")
+    stock = models.PositiveIntegerField(blank=True, null=True, help_text="Amount of stock present at location.")
+    date = models.DateTimeField()
+    # this could be a boolean, but is more readable this way
+    type = models.CharField(max_length=1, choices=TRANSACTION_TYPES)
+    status = models.CharField(max_length=1, choices=STATUS_TYPES)
+    flag = models.CharField(blank=True, null=True, max_length=1, choices=FLAG_TYPES)
+    
+    def _get_transaction(self):
+        if self.status == 'C':
+            if self.type == 'I':
+                return Transaction.objects.filter(issue__pk=self.pk)
+            elif self.type == 'R':
+                return Transaction.objects.filter(receipt__pk=self.pk)
+
+    # there should only ever be one transaction for a partial transaction,
+    # but since this returns a queryset, the property name is plural
+    transactions = property(_get_transaction)
+
+    class Meta:
+        ordering = ['-status']
+
+
+class Shipment(models.Model):
+    '''Stores vaccine shipments'''
+    origin = models.ForeignKey(Facility, related_name='vlm_shipments_origin')
+    destination = models.ForeignKey(Facility, related_name='vlm_shipments_destination')
+    commodity = models.CharField(blank=True, null=True, max_length=10, choices=PartialTransaction.COMMODITIES)
+    expiry = models.DateField(db_index=True)
+    sent = models.DateTimeField()
+    received = models.DateTimeField()
+    batch = models.CharField(max_length=15, blank=True, null=True, help_text="Batch number")
+
+    def __unicode__(self):
+        return "%s (%s) ==> %s (%s)" % (self.origin.name, self.sent.date(), self.destination.name, self.received.date())
+
+class Stock(models.Model):
+    facility = models.ForeignKey(Facility, related_name="stock")
+    balance = models.PositiveIntegerField(blank=True, null=True, help_text="Amount of supply at warehouse")
+
+    def __unicode__(self):
+        return "%s (%s doses)" % (self.facility, self.balance)
+
+
+class Transaction(models.Model):
+    FLAG_TYPES = (
+        ('A', 'Mis-matched amounts'),
+        ('W', 'Mis-matched waybill'),
+        ('I', 'Incorrect. Has been replaced.'),
+    )
+
+    amount_sent  = models.PositiveIntegerField(blank=True, null=True, help_text="Amount of supply shipped from origin")
+    amount_received = models.PositiveIntegerField(blank=True, null=True, help_text="Amount of supply received by destination")
+    shipment = models.ForeignKey(Shipment)
+    issue = models.ForeignKey(PartialTransaction, related_name='issues')
+    receipt = models.ForeignKey(PartialTransaction, related_name='receipts')
+    flag = models.CharField(blank=True, null=True, max_length=1, choices=FLAG_TYPES)
+
+    def __unicode__(self):
+        return "%s (%s) ==> %s (%s)" % (self.shipment.origin.name, self.amount_sent, self.shipment.destination.name, self.amount_received)
+
