843c7cac6d765a21704108fb7755c40a9f5e5f7e
Tim Akinbo <takinbo@gmail.com>
2010-11-02 08:26:12 +0100

    updates to the stock display dashboard

--- apps/vlm/views.py
+++ apps/vlm/views.py
@@ -15,15 +15,16 @@ def dashboard(req):
 
 # default to Kaduna State
 def state_summary(req, state=19, year=0, month=0):
+    all_commodities = dict(PartialTransaction.COMMODITIES)
     month_year = datetime.now().year if not year else int(year)
     month_month = datetime.now().month if not month else int(month)
 
     start_period = datetime(year=month_year, month=month_month, day=1)
-    end_period = datetime(year=month_year, month=month_month + 1, day=1) - timedelta(0, 0, 1)
+    end_period = datetime(year=month_year, month=month_month + 1, day=1)
     state_stock_balance = []
-    state_stock = Stock.objects.filter(facility__code=state)
+    state_stock = Stock.objects.filter(facility__code=state, time__gte=start_period, time__lt=end_period)
     for stock in state_stock:
-        state_stock_balance.append({'commodity': PartialTransaction.COMMODITIES[int(stock.commodity) - 1][1], 'balance': stock.balance})
+        state_stock_balance.append({'commodity': all_commodities[stock.commodity], 'balance': stock.balance})
 
     stock_balances = []
 
@@ -32,11 +33,11 @@ def state_summary(req, state=19, year=0, month=0):
     commodities_dict = dict([[x, 0] for x in commodities])
     for lga in lgas:
         lga_commodities = commodities_dict.copy()
-        lga_commodities.update(dict(Stock.objects.filter(facility__location=lga).values_list('commodity', 'balance')))
-        stock_balances.append({'lga':lga, 'commodities': lga_commodities})
+        lga_commodities.update(dict(Stock.objects.filter(facility__location=lga, time__gte=start_period, time__lt=end_period).values_list('commodity', 'balance')))
+        stock_balances.append({'lga':lga.name, 'commodities': lga_commodities})
         
     return render_to_response(req, "vlm/vlm_state_summary.html", {
         'stock': state_stock_balance, 'state_id': state, 'month_year': month_year,
         'month_month': month_month, 'commodities': commodities, 
-        'lga_stock': stock_balances
+        'lga_stock': stock_balances, 'commodity_names': all_commodities,
     })
