27e12546c3a57798aeff13ab15894d8bf1622c57
Tim Akinbo <takinbo@timbaobjects.com>
2011-02-12 21:27:06 +0100

    modified the vlm views to only retrieve the latest available data for each commodity

--- apps/vlm/views.py
+++ apps/vlm/views.py
@@ -4,9 +4,6 @@
 from models import *
 from rapidsms.webui.utils import render_to_response
 from datetime import datetime
-from django.db.models import Max
-# The import here newly added for serializations
-
 
 def _compare(x, y):
     if x['commodity_code'] == y['commodity_code']:
@@ -15,6 +12,20 @@ def _compare(x, y):
         return -1
     else:
         return 1
+
+''' the filter_stock method returns only the first of a particular
+commodity in a list of commodity list data'''
+def filter_stock(stock_list):
+    old_list = stock_list
+    new_list = []
+    commodity_map = {}
+    for item in old_list:
+        key = 'commodity_code' if item.has_key('commodity_code') else 'commodity'
+        if not commodity_map.has_key(item[key]):
+            new_list.append(item)
+            commodity_map[item[key]] = True
+
+    return new_list
 
 def dashboard(req):
     all_commodities = dict(PartialTransaction.COMMODITIES)
@@ -30,16 +41,16 @@ def dashboard(req):
         end_period = datetime(year=month_year, month=month_month + 1, day=1)
 
     location_stock_balance = []
-    location_stock = Stock.objects.filter(facility__code=location, time__gte=start_period, time__lt=end_period).annotate(time=Max('time'))
+    location_stock = Stock.objects.filter(facility__code=location, time__gte=start_period, time__lt=end_period).order_by('-time')
     if location_stock:
         stock_date = location_stock[0].time
     else:
         stock_date = None
-    location_stock.query.group_by=['commodity']
         
     for stock in location_stock:
         location_stock_balance.append({'commodity_code': stock.commodity, 'commodity': all_commodities[stock.commodity], 'balance': stock.balance})
 
+    location_stock_balance = filter_stock(location_stock_balance)
     location_stock_balance.sort(_compare)
 
     stock_balances = []
@@ -49,13 +60,25 @@ def dashboard(req):
     commodities_dict = dict([[x, 0] for x in commodities])
     for sub_location in sub_locations:
         sub_location_commodities = commodities_dict.copy()
-        sub_location_stock = Stock.objects.filter(facility__location=sub_location, time__gte=start_period, time__lt=end_period).annotate(time=Max('time'))
+        sub_location_stock = Stock.objects.filter(facility__location=sub_location, time__gte=start_period, time__lt=end_period).order_by('-time')
         if sub_location_stock:
             sub_location_stock_date = sub_location_stock[0].time
         else:
             sub_location_stock_date = None
-        sub_location_stock.query.group_by = ['commodity']
-        sub_location_commodities.update(dict(sub_location_stock.values_list('commodity', 'balance')))
+
+        sub_location_stock_balance = []
+        '''The following fetches all the stock balances and updates the commodities list
+        with the latest available stock balance for each commodity'''
+        for stock in sub_location_stock:
+            sub_location_stock_balance.append({'commodity': stock.commodity, 'balance': stock.balance})
+
+        # filter the stock balance and return only the latest
+        sub_location_stock_balance = filter_stock(sub_location_stock_balance)
+
+        # do the update
+        for stock_balance in sub_location_stock_balance:
+            sub_location_commodities[stock_balance['commodity']] = stock_balance['balance']
+
         stock_balances.append({'name':sub_location.name, 'latest': sub_location_stock_date, 'commodities': sub_location_commodities})
         
     return render_to_response(req, "vlm/vlm_dashboard.html", {
@@ -77,15 +100,14 @@ def location_summary(req, zone="", state="", year=0, month=0):
 
     location_stock_balance = []
     if state:
-        location_stock = Stock.objects.filter(facility__code=state, time__gte=start_period, time__lt=end_period).annotate(time=Max('time'))
-        location_stock.query.group_by=['commodity']
+        location_stock = Stock.objects.filter(facility__code=state, time__gte=start_period, time__lt=end_period).order_by('-time')
     else:
-        location_stock = Stock.objects.filter(facility__code=zone, time__gte=start_period, time__lt=end_period).annotate(time=Max('time'))
-        location_stock.query.group_by=['commodity']
+        location_stock = Stock.objects.filter(facility__code=zone, time__gte=start_period, time__lt=end_period).order_by('-time')
         
     for stock in location_stock:
         location_stock_balance.append({'commodity_code': stock.commodity, 'commodity': all_commodities[stock.commodity], 'balance': stock.balance})
 
+    location_stock_balance = filter_stock(location_stock_balance)
     location_stock_balance.sort(_compare)
 
     stock_balances = []
@@ -93,11 +115,24 @@ def location_summary(req, zone="", state="", year=0, month=0):
     sub_locations = Facility.objects.get(code=location).location.children.all()
     commodities = Stock.objects.filter(facility__location__in=sub_locations).order_by('commodity').values_list('commodity', flat=True).distinct()
     commodities_dict = dict([[x, 0] for x in commodities])
+
     for sub_location in sub_locations:
         sub_location_commodities = commodities_dict.copy()
-        sub_location_stock = Stock.objects.filter(facility__location=sub_location, time__gte=start_period, time__lt=end_period).annotate(time=Max('time'))
-        sub_location_stock.query.group_by = ['commodity']
-        sub_location_commodities.update(dict(sub_location_stock.values_list('commodity', 'balance')))
+        sub_location_stock = Stock.objects.filter(facility__location=sub_location, time__gte=start_period, time__lt=end_period).order_by('-time')
+
+        sub_location_stock_balance = []
+        '''The following fetches all the stock balances and updates the commodities list
+        with the latest available stock balance for each commodity'''
+        for stock in sub_location_stock:
+            sub_location_stock_balance.append({'commodity': stock.commodity, 'balance': stock.balance})
+
+        # filter the stock balance and return only the latest
+        sub_location_stock_balance = filter_stock(sub_location_stock_balance)
+
+        # do the update
+        for stock_balance in sub_location_stock_balance:
+            sub_location_commodities[stock_balance['commodity']] = stock_balance['balance']
+
         stock_balances.append({'sub_location':sub_location.name, 'commodities': sub_location_commodities})
         
     return render_to_response(req, "vlm/vlm_location_summary.html", {
